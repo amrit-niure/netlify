5a9e1b6844091b1f784a1a8ff485b7fa91410fd1
Amrit Niure <niureamrit2@gmail.com>
2023-05-06 10:24:39 +1000

    Added Home link

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/">Home</Link>
-          <Link to="/about">Home</Link>
+          <Link to="/about">About</Link>
         </nav>
       </header>
         <Routes>
